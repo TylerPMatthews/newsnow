f4984f4a3c8697407a6255ade7381fb0143a165e
ty <tylerpmatthews1998@gmail.com>
2020-12-23 22:10:05 -0500

    test

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React from "react";
 import "./App.css";
 import Home from "./components/Home";
 import { Route, Switch } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Sarticles from "./components/Sarticles";
-import Intro from './components/intro/Intro';
+import Intro from "./components/intro/Intro";
 function App() {
   return (
     <div>
       <Switch>
-       
-        <Route path='/home'>
+        <Route path="/home">
           <Home />
         </Route>
         <Route exact path="/">
